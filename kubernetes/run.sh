44d7871bbbf575f61dc1833903155ed2845528b6
alfredobs97 <alfredobautista1@gmail.com>
2019-07-13 14:32:22 +0200

    Explain how to connect

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -5,6 +5,10 @@ kubectl apply -f service.yaml
 
 echo ""
 
-echo 'This services use NodePort to connect with server, use kubernetes cluster ip and this port'
+echo 'This services use NodePort to connect with server, use kubernetes cluster external ip and this port'
 
 kubectl get services | grep transfer | awk '{print $5}' | awk -F ":" '{print $2}'
+
+
+echo ""
+echo 'if you use Minikube to develop, use minikube ip to connect with previus port'
